7a6f3aea8ab4fa39cbd62b50b837fe5a16dda8c0
Luna-bf <lburattifayolle@gmail.com>
2024-10-18 15:06:32 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,12 +1,28 @@
-$(window).scroll(function() {
-    if ($(this).scrollTop() >= 50) {
-        $('#return-to-top').fadeIn(200);
-    } else {
-        $('#return-to-top').fadeOut(200);
+$(document).ready(function() {
+    var animation_elements = $.find('.animation-item');
+    var web_window = $(window);
+    
+    function check_if_in_view() {
+        var window_height = web_window.height();
+        var window_top_position = web_window.scrollTop();
+        var window_bottom_position = (window_top_position + window_height);
+        
+        $.each(animation_elements, function() {
+            var element = $(this);
+            var element_height = $(element).outerHeight();
+            var element_top_position = $(element).offset().top;
+            var element_bottom_position = (element_top_position + element_height);
+        
+            
+            if((element_bottom_position >= window_top_position) && (element_top_position <= window_bottom_position)) {
+                element.addClass('item-in-view');
+            } else {
+                element.removeClass('item-in-view');
+            }
+        });
     }
-});
-$('#return-to-top').click(function() {
-    $('body,html').animate({
-        scrollTop : 0
-    }, 500);
+    $(window).on('scroll resize', function() {
+        check_if_in_view();
+    });
+    $(window).trigger('scroll');
 });
