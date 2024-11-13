ce5203705891828953d50cbf0606ed150aa3c083
Luna-bf <lburattifayolle@gmail.com>
2024-11-13 13:04:55 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,17 +1,31 @@
+const dropdowns = document.querySelectorAll('.dropdown');
+        console.log(dropdowns);
+        
+        for(let i = 0; i < dropdowns.length; i++) {
+            
+            dropdowns[i].addEventListener('click', function(e) {
+                
+                //for(let x = 0; i < dropdowns.length; x++) {
+                  //  dropdowns[x].querySelector('.dropdown-content').classList.add('hide');
+                //}
+                e.currentTarget.querySelector('.dropdown-content').classList.toggle('hide');
+            });
+        }
+
 $(document).ready(function() {
-    var animation_elements = $.find('.animation-item');
-    var web_window = $(window);
+    let animation_elements = $.find('.animation-item');
+    let web_window = $(window);
     
     function check_if_in_view() {
-        var window_height = web_window.height();
-        var window_top_position = web_window.scrollTop();
-        var window_bottom_position = (window_top_position + window_height);
+        let window_height = web_window.height();
+        let window_top_position = web_window.scrollTop();
+        let window_bottom_position = (window_top_position + window_height);
         
         $.each(animation_elements, function() {
-            var element = $(this);
-            var element_height = $(element).outerHeight();
-            var element_top_position = $(element).offset().top;
-            var element_bottom_position = (element_top_position + element_height);
+            let element = $(this);
+            let element_height = $(element).outerHeight();
+            let element_top_position = $(element).offset().top;
+            let element_bottom_position = (element_top_position + element_height);
         
             
             if((element_bottom_position >= window_top_position) && (element_top_position <= window_bottom_position)) {
