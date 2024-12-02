80c9e1bf0263e99e9225d483c940691fc28a1a71
Luna-bf <lburattifayolle@gmail.com>
2024-12-02 15:29:09 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,11 +1,32 @@
-const dropdown = document.querySelectorAll('.dropdown');
-        
-//Boucle for pour la boîte de changement de langue
-for(let i = 0; i < dropdown.length; i++) {
-    dropdown[i].addEventListener('click', function(e) {
-        for(let h = 0; h < dropdown.length; h++) {
-          dropdown[h].querySelector('.dropdown-content').classList.add('hide');
-        }
-        e.currentTarget.querySelector('.dropdown-content').classList.toggle('hide');
-    });
-}
+//Je récupère mes éléments
+let darkMode = window.localStorage.getItem("dark-mode");
+const changeTheme = document.getElementById("change-theme-btn");
+
+//Je déclare mes fonctions enableDarkMode et disableDarkMode
+const enableDarkMode = () => {
+    
+    document.body.classList.add("dark-mode");
+    window.localStorage.setItem("dark-mode", "active"); //Je ne peux mettre que des strings dans le localStorage, au lieu de mettre isActive (true ou false) je met juste 'active'
+};
+
+const disableDarkMode = () => {
+    
+    document.body.classList.remove("dark-mode");
+    window.localStorage.setItem("dark-mode", null);
+};
+
+//Puis je met un event listener sur le bouton, ce qui va me permettre de changer de thème
+changeTheme.addEventListener("click", () => {
+    
+    darkMode = window.localStorage.getItem("dark-mode");
+    darkMode !== "active" ? enableDarkMode() : disableDarkMode();
+    
+    //C'est l'équivalent de :
+    /*
+    if(darkMode !== "isActive") {
+        enableDarkMode();
+    } else {
+        disableDarkMode();
+    }
+    */
+});
