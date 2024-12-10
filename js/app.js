98174e03f3b19dd1aa2c663074bb87b14c91020a
Luna-bf <lburattifayolle@gmail.com>
2024-12-10 14:26:36 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -2,10 +2,16 @@
 const elements = {
     darkMode: window.localStorage.getItem("dark-mode"),
     changeTheme: document.getElementById("change-theme-btn"),
+    increment: document.getElementById("increment"),
+    decrement: document.getElementById("decrement"),
+    reset: document.getElementById("reset-all"),
     btn: document.querySelector('#prompt button'),
     prompt: document.getElementById("prompt"),
-    paragraph: document.querySelector("#prompt p")
+    paragraph: document.querySelector("#prompt p"),
 };
+
+
+/*Dark mode*/
 
 //Je déclare mes fonctions enableDarkMode et disableDarkMode
 const enableDarkMode = () => {
@@ -36,12 +42,29 @@ elements.changeTheme.addEventListener("click", () => {
     */
 });
 
+
+/*Accessibilité*/
+elements.increment.addEventListener("click", () => {
+    document.querySelector("body").style.fontSize = "24px";
+});
+
+elements.decrement.addEventListener("click", () => {
+    document.querySelector("body").style.fontSize = "16px";
+});
+
+elements.reset.addEventListener("click", () => {
+    document.querySelector("body").style.fontSize = "16px";
+    elements.darkMode = disableDarkMode();
+});
+
+
+
 //Apparition du prompt
-setTimeout(function() {
+/*setTimeout(function() {
     elements.prompt.classList.remove('hide-prompt');
 }, 5000);
 
 //Pour faire disparaitre le prompt
 elements.btn.addEventListener('click', () => {
     elements.prompt.classList.add('hide-prompt');
-});
+});*/
