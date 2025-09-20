121a33701b45fed4175807abd6e82f4206a7ef1e
Luna-bf <lburattifayolle@gmail.com>
2025-09-20 08:58:59 +0200

    Updated responsive

--- assets/js/app.js
+++ assets/js/app.js
@@ -29,3 +29,17 @@ new Swiper('.card-wrapper', {
         },
     }
 });
+
+
+// Cover background (active burger menu)
+const coverBackground = document.getElementById('cover-background');
+const burgerMenuBtn = document.querySelector('.open-menu');
+
+burgerMenuBtn.addEventListener('click', function () {
+
+    // Faire en sorte que le fond sois sombre uniquement lorsque le burger menu est ouvert
+    coverBackground.classList.toggle('covering');
+    window.setTimeout(function () { coverBackground.classList.toggle('covering'); }, 5000);
+
+    // if(coverBackground.classList)
+});
