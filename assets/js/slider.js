e1b1c171e3f64dce2c8659e5ed58d87b8f4d7b9e
Luna-bf <lburattifayolle@gmail.com>
2025-10-01 13:24:19 +0200

    new js file for the slider

---
+++ assets/js/slider.js
@@ -0,0 +1,31 @@
+// Slider
+new Swiper('.card-wrapper', {
+    loop: true,
+    spaceBetween: 40,
+
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+        dynamicBullets: true,
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    // Responsive
+    breakpoints: {
+        0: {
+            slidesPerView: 1
+        },
+        768: {
+            slidesPerView: 2
+        },
+        1024: {
+            slidesPerView: 3
+        },
+    }
+});
