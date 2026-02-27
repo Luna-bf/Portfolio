fb78694aa4e0d76bc32d96ccab79559aff589b93
Luna-bf <lburattifayolle@gmail.com>
2026-02-27 16:19:36 +0100

    Màj du carrousel : ajout de commentaire et d'un écouteur d'événement (DOMContentLoaded)

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -1,31 +1,36 @@
-// Slider
-new Swiper('.card-wrapper', {
-    loop: true,
-    spaceBetween: 40,
+document.addEventListener("DOMContentLoaded", () => {
 
-    // If we need pagination
-    pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-        dynamicBullets: true,
-    },
+    // Initialisation du carrousel
+    const swiper = new Swiper('.card-wrapper', {
+        // modules: [Navigation, Pagination],
 
-    // Navigation arrows
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
+        loop: true,
+        spaceBetween: 40,
 
-    // Responsive
-    breakpoints: {
-        0: {
-            slidesPerView: 1
+        // Pagination (points sous le slider)
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+            dynamicBullets: true,
         },
-        768: {
-            slidesPerView: 2
+
+        // Flèche de navigation (pour utiliser le carrousel)
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
         },
-        1024: {
-            slidesPerView: 3
-        },
-    }
+
+        // Responsive : défini combien de diapositives je vais afficher selon la taille de l'écran
+        breakpoints: {
+            0: {
+                slidesPerView: 1
+            },
+            768: {
+                slidesPerView: 2
+            },
+            1024: {
+                slidesPerView: 3
+            },
+        }
+    });
 });
